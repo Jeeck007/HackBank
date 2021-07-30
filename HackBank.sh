09d5576c2d16d240d47223303adc56d7fe41e990
Jeeck007 <88170751+Jeeck007@users.noreply.github.com>
2021-07-30 17:39:39 +0700

    Add files via upload

---
+++ HackBank.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# tampilan
+toilet -f big -F gay Hack Bank
+echo
+echo "================================" | lolcat
+echo " Author : Mr.Jeeck "
+echo " Whatsapp : 081392505882 "
+echo " Facebook : Jecko X Ganz "
+echo "================================" | lolcat
+echo
+echo "Masukan Nama Bank Contoh ( BRI,BCA,BNI )" | lolcat
+read -p "$" bank
+echo
+sleep 3
+echo
+echo "Masukan Nomor Rekening " | lolcat
+read -p "$" norek
+sleep 3
+echo
+echo "Masukan Nominal Transfer" | lolcat
+read -p "$" masuk
+sleep 3
+echo
+echo "$bank nomor $norek telah di transfer $masuk " | lolcat
+echo
+echo "# Gunakan Dengan Bijak " | lolcat
+echo "# Babay " | lolcat
+echo
+
